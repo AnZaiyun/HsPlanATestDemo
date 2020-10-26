cdc8aac7ae192e280b3aa49804ac00c0179846dc
AnZaiyun <1732478192@qq.com>
2020-10-26 11:02:22 +0800

    [更新项目]更新rabbitmq和java的交互

--- src/Rabbitmq/ConsumerFanout.py
+++ src/Rabbitmq/ConsumerFanout.py
@@ -1,20 +1,36 @@
 import pika
 '''https://www.cnblogs.com/shenh/p/10497244.html'''
+from src.SocketTest.LoggingDemo import LoggingFactory
+import requests
 
+logfactory = LoggingFactory('info','debug')
+log = logfactory.getLogger()
 credentials = pika.PlainCredentials('guest', 'guest')
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = '127.0.0.1',port = 5672,virtual_host = '/',credentials = credentials))
+log.info('连接队列...')
 channel = connection.channel()
 # 创建临时队列,队列名传空字符，consumer关闭后，队列自动删除
 # exclusive=False,durable=True
-result = channel.queue_declare('rabbitmq_queue',exclusive=False,durable=True)
+result = channel.queue_declare('QUEUE_C',exclusive=False,durable=True)
 # 声明exchange，由exchange指定消息在哪个队列传递，如不存在，则创建。durable = True 代表exchange持久化存储，False 非持久化存储
-channel.exchange_declare(exchange = 'rabbitmq_queue',durable = True, exchange_type='fanout')
+channel.exchange_declare(exchange = 'my-mq-exchange_C',durable = True, exchange_type='fanout')
 # 绑定exchange和队列  exchange 使我们能够确切地指定消息应该到哪个队列去
-channel.queue_bind(exchange = 'rabbitmq_queue',queue = result.method.queue)
+channel.queue_bind(exchange = 'my-mq-exchange_C',queue = result.method.queue)
 # 定义一个回调函数来处理消息队列中的消息，这里是打印出来
+log.info('获取队列(QUEUE_C)信息...')
 def callback(ch, method, properties, body):
     ch.basic_ack(delivery_tag = method.delivery_tag)
-    print(body.decode())
+    url = str(body.decode())
+    log.info('队列(QUEUE_C)消息为：'+url)
+
+    if url.startswith('http://'):
+        log.info('开始发送请求调用远程接口:'+url)
+
+        # 以下开始构建请求request信息
+        session_requests = requests.session()
+        response_data = session_requests.get(url)
+        log.info('返回数据：'+response_data.text)
+
 
 channel.basic_consume(result.method.queue,callback,# 设置成 False，在调用callback函数时，未收到确认标识，消息会重回队列。True，无论调用callback成功与否，消息都被消费掉
                       auto_ack = False)
--- src/Rabbitmq/ProviderFanout.py
+++ src/Rabbitmq/ProviderFanout.py
@@ -13,12 +13,15 @@ credentials = pika.PlainCredentials('guest', 'guest')  # mq用户名和密码
 # 虚拟队列需要指定参数 virtual_host，如果是默认的可以不填。
 connection = pika.BlockingConnection(pika.ConnectionParameters(host = '127.0.0.1',port = 5672,virtual_host = '/',credentials = credentials))
 channel=connection.channel()
+result = channel.queue_declare(queue='QUEUE_PYTHON',durable= True)
 # 声明exchange，由exchange指定消息在哪个队列传递，如不存在，则创建。durable = True 代表exchange持久化存储，False 非持久化存储
-channel.exchange_declare(exchange = 'python-test',durable = True, exchange_type='fanout')
+channel.exchange_declare(exchange = 'my-mq-exchange_PYTHON',durable = True, exchange_type='direct')
+# 将exchange和queue绑定
+channel.queue_bind(exchange = 'my-mq-exchange_PYTHON',queue = result.method.queue)
 for i in range(10):
     message=json.dumps({'OrderId':"1000%s"%i})
-    # 向队列插入数值 routing_key是队列名。delivery_mode = 2 声明消息在队列中持久化，delivery_mod = 1 消息非持久化。routing_key 不需要配置
-    channel.basic_publish(exchange = 'python-test',routing_key = '',body = message,
+    # 向队列插入数值 routing_key是路由名。delivery_mode = 2 声明消息在队列中持久化，delivery_mod = 1 消息非持久化。
+    channel.basic_publish(exchange = 'my-mq-exchange_PYTHON',routing_key = 'spring-boot-routingKey_Python',body = message,
                           properties=pika.BasicProperties(delivery_mode = 2))
     print(message)
 connection.close()
