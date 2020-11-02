8f97b7a4177cf5f31b644fc01e922922c4273711
AnZaiyun <1732478192@qq.com>
2020-11-02 16:07:17 +0800

    更新scoket监控功能

--- src/SocketTest/SocketHttpService.py
+++ src/SocketTest/SocketHttpService.py
@@ -1,9 +1,16 @@
 from socket import *
 from src.SocketTest.LoggingDemo import LoggingFactory
+import re
+
 HOST = ''
 PORT = 8088
 BUFSIZ = 1024
 ADDR = (HOST, PORT)
+
+response_content ='''
+HTTP/1.1 200 ok
+1231233
+'''
 
 tcpSerSocket = socket(AF_INET, SOCK_STREAM)
 tcpSerSocket.bind(ADDR)
@@ -24,28 +31,26 @@ while True:
     data = data.decode('utf-8')
     log.debug('****全部数据****\r\n'+data)
     if data.find('POST') >=0:
-        params = data.split('\r\n')[-1]
-        params = params.split('&')
-
-        params_dict = dict()
-        for item in params:
-            key, value = item.split('=')
-            params_dict[key] = value
-        log.debug('****请求参数****\r\n'+str(params_dict))
-
-        if 'func_name' in params_dict.keys():
-            func_name = params_dict['func_name']
-            func_args = ''
+        pattern = re.compile(r'func_name[^\w]+(\w+)[^\w]+func_args[^\w]+(\w+)')
+        params = re.search(pattern,data)
+        if params:
             try:
-                func_args = params_dict['func_args']
-            except:
-                log.warning('当前请求未传递参数。。。')
-
-            log.debug('开始执行方法:' + str(func_name) + '  方法参数：' + str(func_args))
+                func_name= params.group(1)
+                func_args = params.group(2)
+                log.debug('****请求参数****\r\n' + "func_name:" + func_name + "\r\nfunc_args:" + func_args)
+                tcpCliSocket.send(response_content.format(func_name).encode())
+            except Exception as ex:
+                log.warning('解析参数出错')
         else:
-            log.debug('当前请求未传递方法。。。')
-            continue
+            log.debug('当前请求未传递参数')
+            try:
+                tcpCliSocket.send(response_content.format(func_name).encode())
+            except Exception:
+                tcpCliSocket.send(response_content.encode())
+                log.warning('当前请求未传递参数')
     else:
         log.debug('非post请求')
+        # tcpCliSocket.send("index_content".encode())
+        tcpCliSocket.sendall(response_content.format("非post请求").encode())
 
 tcpSerSocket.close()
